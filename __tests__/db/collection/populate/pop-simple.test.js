36b05b445c27ea9a5accabc83a554fab23ac022f
fabiantoth <fabiantoth@gmail.com>
2021-05-05 10:55:51 -0500

    refactor populate tests for simplified version of populate feature

--- __tests__/db/collection/populate/pop-simple.test.js
+++ __tests__/db/collection/populate/pop-simple.test.js
@@ -17,6 +17,7 @@ const dbSettings = {
 
 let db
 let groupsRef
+let articlesRef
 let usersRef
 
 beforeAll(async (done) => {
@@ -24,6 +25,7 @@ beforeAll(async (done) => {
     db = new DB('pop-simple')
 
     await db.addCollection('groups')
+    await db.addCollection('articles')
     await db.addCollection('users')
 
     const GroupSchema = new Schema({
@@ -33,11 +35,23 @@ beforeAll(async (done) => {
     db.addSchema('Group', GroupSchema)
     groupsRef = db.collection('groups').useModel('Group')
 
+    const ArticleSchema = new Schema({
+        title: String
+    })
+
+    db.addSchema('Article', ArticleSchema)
+    articlesRef = db.collection('articles').useModel('Article')
+
     await groupsRef.insertMany([
         { title: 'Group 1' },
         { title: 'Group 2' },
         { title: 'Group 3' },
         { title: 'Group 4' }
+    ])
+
+    await articlesRef.insertMany([
+        { title: 'Article 1' },
+        { title: 'Article 2' }
     ])
 
     const User = new Schema({
@@ -50,16 +64,9 @@ beforeAll(async (done) => {
             collection: 'groups',
             $ref: Number
         }],
-        nested: {
-            record: Number,
-            ref: {
-                collection: 'groups',
-                $ref: Number
-            },
-            refsArr: [{
-                collection: 'groups',
-                $ref: Number
-            }]
+        articleRef: {
+            collection: 'articles',
+            $ref: Number
         }
     })
 
@@ -84,36 +91,35 @@ beforeEach(async () => {
 
 test('0 -> setup: #document add parent documents', async (done) => {
     let userRes = await usersRef.insertMany([
-        { name: 'John', ref: 1, refsArr: [1,2,3,4] },
-        { name: 'Jane', ref: 2, refsArr: [1,2] },
-        { name: 'sally', nested: { record: 1, refsArr: [3] } },
-        { name: 'tom', nested: { record: 1, ref: 2 } }
+        { name: 'John', ref: 1, refsArr: [1,2,3,4], articleRef: 1 },
+        { name: 'Jane', ref: 2, refsArr: [1,2], articleRef: 2 }
     ])
     done()
 })
 
 test('1 -> Collection.populate(): #ref should populate $ref embed', async (done) => {
-    let userRes = await usersRef.where('id != $undefined').populate(['ref']).find()
-    // let res = userRes.data
-    // console.log(res)
-    // expect.objectContaining({
-    //     id: expect(res[0].id).toBe(1),
-    //     ref: expect(res[0].ref).toEqual(expect.objectContaining({ id: 1, title: 'Group 1' }))
-    // })
+    let userRes = await usersRef.where('id != $undefined').populate(['ref', 'refsArr', 'articleRef']).find()
+    let res = userRes.data
+    expect.objectContaining({
+        id: expect(res[0].id).toBe(1),
+        refsArr: expect(res[0].refsArr).toEqual(expect.arrayContaining([
+            { id: 1, title: 'Group 1' },
+            { id: 2, title: 'Group 2' },
+            { id: 3, title: 'Group 3' },
+            { id: 4, title: 'Group 4' }
+        ])),
+        articleRef: expect(res[0].articleRef).toEqual(expect.objectContaining({ id: 1, title: 'Article 1' }))
+    })
+    expect.objectContaining({
+        id: expect(res[1].id).toBe(2),
+        refsArr: expect(res[1].refsArr).toEqual(expect.arrayContaining([
+            { id: 1, title: 'Group 1' },
+            { id: 2, title: 'Group 2' }
+        ])),
+        articleRef: expect(res[1].articleRef).toEqual(expect.objectContaining({ id: 2, title: 'Article 2' }))
+    })
     done()
 })
-
-test('2 -> Collection.populate(): #array[#ref] should populate array of $refs', async (done) => {
-    let userRes = await usersRef.where('refsArr.length > 0').populate(['refsArr']).find()
-    // let res = userRes.data
-    // console.log(res)
-    // expect.objectContaining({
-    //     id: expect(res[0].id).toBe(1),
-    //     ref: expect(res[0].ref).toEqual(expect.objectContaining({ id: 1, title: 'Group 1' }))
-    // })
-    done()
-})
-
 
 
 
@@ -129,20 +135,11 @@ test('(-1) -> Collection.populate(): #error should throw error when path does no
     }))
 })
 
-test('(-2) -> Collection.populate(): #error #nestedObject should throw error when path does not point to $ref embed', () => {
+test('(-2) -> Collection.populate(): #error #schema should throw error when path does not point to schema path', () => {
     expect.assertions(1)
     return usersRef.where('id != $undefined').populate(['nested.record']).find()
     .catch(e => expect(e).toEqual({
         "error": true,
-        "message": `Cannot populate path 'nested.record' because it is not a $ref type`
-    }))
-})
-
-test('(-3) -> Collection.populate(): #error #nestedObject should throw error when path does not point to $ref embed', () => {
-    expect.assertions(1)
-    return usersRef.where('id != $undefined').populate(['nested.notRef']).find()
-    .catch(e => expect(e).toEqual({
-        "error": true,
-        "message": `No schema type set at path 'nested.notRef'`
+        "message": `No schema type set at path 'nested.record'`
     }))
 })
