6699c3f37f3558973c0daead194dbeb187b9f9e1
fabiantoth <fabiantoth@gmail.com>
2021-06-10 17:35:35 -0500

    add negative tests for bad query status response

--- __tests__/api/server.default-schema.test.js
+++ __tests__/api/server.default-schema.test.js
@@ -108,16 +108,19 @@ afterAll(async (done) => {
         .catch(e => console.log(e))  
 })
 
-test('1 -> Server: POST /api/db/:name - Should create a new collection', async () => {
+test('1 -> POST /api/db/:name - Should create new collection', async (done) => {
     await request(appServer('defaultschema', 'api')).post('/api/db/users').send({
         fileSize: 10000,
         idType: '$incr',
         idCount: 0,
         idMaxCount: 10000
     }).expect(201)
+
+    await request(appServer('defaultschema', 'api')).post('/api/db/groups').send().expect(201)
+    done()
 })
 
-test('2 -> Server: POST /api/collection - Should add 4 new documents', async (done) => {
+test('2 -> POST /api/collection - Should add 4 new documents', async (done) => {
     setTimeout(async () => {
         let res = await request(appServer('defaultschema', 'api')).post('/api/users').send(documents).expect(201)
         expect(res.body.data.length).toBe(4)
@@ -125,7 +128,7 @@ test('2 -> Server: POST /api/collection - Should add 4 new documents', async (do
       }, 50)
 })
 
-test('3 -> Server: GET /api/collection/:id - Should get 1 document', async (done) => {
+test('3 -> GET /api/collection/:id - Should get 1 document', async (done) => {
     setTimeout(async () => {
         let response = await request(appServer('defaultschema', 'api')).get('/api/users/3').send().expect(200)
         let res = response.body.data
@@ -147,7 +150,7 @@ test('3 -> Server: GET /api/collection/:id - Should get 1 document', async (done
       }, 50)
 })
 
-test('4 -> Server: GET /api/collection - Should get all 4 documents', async (done) => {
+test('4 -> GET /api/collection - Should get all 4 documents', async (done) => {
     setTimeout(async () => {
         let res = await request(appServer('defaultschema', 'api')).get('/api/users').send().expect(200)
         expect(res.body.data.length).toBe(4)
@@ -155,7 +158,7 @@ test('4 -> Server: GET /api/collection - Should get all 4 documents', async (don
       }, 50)
 })
 
-test('5 -> Server: PUT /api/collection - Should update 2 documents', async (done) => {
+test('5 -> PUT /api/collection - Should update 2 documents', async (done) => {
     const updates = [
         {
             id: 3,
@@ -201,7 +204,7 @@ test('5 -> Server: PUT /api/collection - Should update 2 documents', async (done
 })
 
 
-test('6 -> Server: GET /api/collection/_q/? - where() queries', async (done) => {
+test('6 -> GET /api/collection/_q/? - where() queries', async (done) => {
     const query = `Document with id "2" has been removed`
 
     setTimeout(async () => {
@@ -220,7 +223,7 @@ test('6 -> Server: GET /api/collection/_q/? - where() queries', async (done) => 
       }, 50)
 })
 
-test('7 -> Server: GET /api/collection/_q/? - where().and() queries', async (done) => {
+test('7 -> GET /api/collection/_q/? - where().and() queries', async (done) => {
     const query = '?where=id,!=,1&where=and&where=age,>,18'
     const query1 = '?where=id,!=,1&where=and&where=active,=,$true'
     const query2 = '?where=id,!=,1&where=and&where=nullValue,=,$null'
@@ -243,7 +246,7 @@ test('7 -> Server: GET /api/collection/_q/? - where().and() queries', async (don
       }, 50)
 })
 
-test('8 -> Server: GET /api/collection/_q/? - where().or() queries', async (done) => {
+test('8 -> GET /api/collection/_q/? - where().or() queries', async (done) => {
     const query = '?where=nullValue,=,$null&where=or&where=active,=,$false'
 
     setTimeout(async () => {
@@ -253,7 +256,7 @@ test('8 -> Server: GET /api/collection/_q/? - where().or() queries', async (done
       }, 50)
 })
 
-test('9 -> Server: GET /api/collection/_q/? - where().and().or() queries', async (done) => {
+test('9 -> GET /api/collection/_q/? - where().and().or() queries', async (done) => {
     const query = '?where=active,=,$true&where=and&where=emptyArray.length,>,2&where=or&where=age,=,18'
 
     setTimeout(async () => {
@@ -263,7 +266,7 @@ test('9 -> Server: GET /api/collection/_q/? - where().and().or() queries', async
       }, 50)
 })
 
-test('10 -> Server: GET /api/collection/_q/? - where(exp, filterFn) queries', async (done) => {
+test('10 -> GET /api/collection/_q/? - where(exp, filterFn) queries', async (done) => {
     const query = '?whereArray=objArr,[title,=,"title 1"]'
     const query1 = '?whereArray=emptyArray,[$item,=,2]'
     const query2 = '?whereArray=emptyArray,[$item,>,2]'
@@ -287,7 +290,7 @@ test('10 -> Server: GET /api/collection/_q/? - where(exp, filterFn) queries', as
       }, 50)
 })
 
-test('11 -> Server: GET /api/collection/_q/? - where(exp, filterFn).where() queries', async (done) => {
+test('11 -> GET /api/collection/_q/? - where(exp, filterFn).where() queries', async (done) => {
     const query = `?whereArray=objArr,[title,=,"title 1"]&where=age,>,16`
     const query1 = '?where=age,>,16&whereArray=objArr,[title,=,"title 1"]'
     const query2 = '?where=age,>,16&where=and,&where=id,>=,1&whereArray=objArr,[title,=,"title 1"]'
@@ -305,7 +308,7 @@ test('11 -> Server: GET /api/collection/_q/? - where(exp, filterFn).where() quer
       }, 50)
 })
 
-test('12 -> : GET /api/collection/_q/? - where()/.include()/.exclude() queries', async (done) => {
+test('12 -> GET /api/collection/_q/? - where()/.include()/.exclude() queries', async (done) => {
     const query = "?where=id,=,1&include=[firstname,lastname,age]"
     const query1 = '?whereArray=objArr,[title,=,"title 3"]&include=[firstname,lastname,age]'
     const query2 = '?where=id,=,1&exclude=[email,emptyArray,strArr,objArr,nullValue,active,joined,access,info]'
@@ -344,7 +347,7 @@ test('12 -> : GET /api/collection/_q/? - where()/.include()/.exclude() queries',
       }, 50)
 })
 
-test('13 -> Server: GET /api/collection/_q/? - where()/.limit()/.offset() queries', async (done) => {
+test('13 -> GET /api/collection/_q/? - where()/.limit()/.offset() queries', async (done) => {
     const query = '?where=id,>,0&limit=2'
     const query1 = '?where=id,>,0&offset=3'
     const query2 = '?where=id,>,0&offset=2&limit=1'
@@ -362,7 +365,7 @@ test('13 -> Server: GET /api/collection/_q/? - where()/.limit()/.offset() querie
       }, 50)
 })
 
-test('14 -> Server: GET /api/collection/_q/? - where()/sort() queries', async (done) => {
+test('14 -> GET /api/collection/_q/? - where()/sort() queries', async (done) => {
     const query = '?where=id,>,0&sortBy=id'
     const query1 = '?where=id,>,0&sortBy=id&sortOrder=desc'
     const query2 = '?where=id,>,0&sortBy=id&sortOrder=asc'
@@ -386,7 +389,7 @@ test('14 -> Server: GET /api/collection/_q/? - where()/sort() queries', async (d
       }, 50)
 })
 
-test('15 -> Server: DELETE /api/collection/:id - Should delete 1 document', async (done) => {
+test('15 -> DELETE /api/collection/:id - Should delete 1 document', async (done) => {
     const expected = `Document with id "2" has been removed`
 
     setTimeout(async () => {
@@ -395,9 +398,48 @@ test('15 -> Server: DELETE /api/collection/:id - Should delete 1 document', asyn
       }, 50)
 })
 
-test('16 -> Server: DELETE /api/db/collection - Should delete collection', async (done) => {
+test('16 -> DELETE /api/db/collection - Should delete collection', async (done) => {
     setTimeout(async () => {
         await request(appServer('defaultschema', 'api')).delete('/api/db/users').expect(200)
         done()
       }, 50)
+})
+
+//
+// ======= negative tests ========== //
+//
+
+test('(-1) -> POST /api/db/:name - return status 400 Bad Request for invalid collection name', async (done) => {
+    await request(appServer('defaultschema', 'api')).post('/api/db/-5').send().expect(400)
+    done()
+})
+
+test('(-2) -> DELETE /api/db/:name - return status 400 Bad Request for invalid collection name', async (done) => {
+    await request(appServer('defaultschema', 'api')).delete('/api/db/-5').send().expect(400)
+    done()
+})
+
+test('(-3) -> GET /api/collection - return status 404 Not Found for collection that does not exist', async (done) => {
+    await request(appServer('defaultschema', 'api')).get('/api/users').send().expect(404)
+    done()
+})
+
+test('(-4) -> GET /api/collection/:id - return status 400 Bad Request for invalid id route', async (done) => {
+    await request(appServer('defaultschema', 'api')).get('/api/groups/test').send().expect(400)
+    done()
+})
+
+test('(-5) -> POST /api/collection - return status 400 Bad Request for empty req.body', async (done) => {
+    await request(appServer('defaultschema', 'api')).post('/api/groups').send().expect(400)
+    done()
+})
+
+test('(-6) -> PUT /api/collection - return status 400 Bad Request for empty req.body', async (done) => {
+    await request(appServer('defaultschema', 'api')).put('/api/groups').send().expect(400)
+    done()
+})
+
+test('(-7) -> DELETE /api/collection/:id - return status 400 Bad Request for invalid id route', async (done) => {
+    await request(appServer('defaultschema', 'api')).delete('/api/groups/test').send().expect(400)
+    done()
 })
